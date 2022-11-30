69ab912aa08fd4cd95d9be668f55ffeec32f8eda
Mark Bieganek <mark.bieganek@telus.com>
2022-11-30 10:34:44 -0700

    App initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * A function that, when given a number, returns whether it is valid or
+ * not (according to the Luhn Algorithm)
+ * @param {number} number - The number to check
+ * @returns {boolean} Will return `true` if the given number is valid
+ */
+const check = (number) => {
+  // Not yet implemented
+};
+
+module.exports = check;
---
+++ test/indexTest.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+
+const check = require('../index');
+
+describe('check', function() {
+  it('should...', function() {
+    const output = check();
+    const expectedOutput = undefined;
+    assert.equal(output, expectedOutput);
+  });
+});
